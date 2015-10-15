f47feb860d9608e9e06fe8dcaf402546b419d54c
Gabriel Abud <gabriel.jabud@gmail.com>
2015-10-15 14:35:25 -0600

    Added other auth logic to page

--- public/scripts/userController.js
+++ public/scripts/userController.js
@@ -3,7 +3,7 @@
     
     angular.module('authApp').controller('UserController', UserController);
     
-    function UserController($http) {
+    function UserController($http, $auth, $rootScope, $state) {
         
         var vm = this;
         
@@ -18,6 +18,24 @@
                vm.users = users.data; 
             }, function(error) {
                vm.error = error.data;
+            });
+        }
+        
+        // We would normally put the logout method in the 
+        // same spot as the login method, ideally extracted out into
+        // a service. For this simpler example we'll leave it here
+        vm.logout = function() {
+            $auth.logout().then(function() {
+               // Remove the authenticated user from local storage
+               localStorage.removeItem('user');
+               
+               // Flip authenticated to false so that we no longer
+               // show UI elements dependant on the user being logged in
+               $rootScope.authenticated = false;
+               
+               // Remove the current user info from rootscope
+               $rootScope.currentUser = null;
+               $state.go('auth');
             });
         }
     }
